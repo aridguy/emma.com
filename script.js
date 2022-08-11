8073cb3c29edb109a6ab8c9b3355d9cd39b28f81
B-usman <babatundeusman82@gmail.com>
2022-08-11 23:40:09 +0100

    usman added a login page

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById('create').onclick = function(){
+   document.getElementById('login').style. display ="none";
+   document.getElementById('signup').style. display ="block"
+}
+
+document.getElementById('signin').onclick = function(){
+    document.getElementById('login').style. display ="block";
+    document.getElementById('signup').style. display ="none"
+ }
